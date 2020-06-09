8264d0e8e4bf4336e9c884f3f3ce8b64a829f414
Marji Symonds <symondsm@oregonstate.edu>
2020-06-09 09:41:19 -0700

    Add board test for to_s and locations

--- lib/game.rb
+++ lib/game.rb
@@ -4,13 +4,17 @@ class Game
 
 	def initialize(*board)
 		@board = board
-		over = false
+		@over = over?
 	end
 
 	def board
 	end
 
 	def over?
+		if @boad.locations = @locations={:top=>{:left=>:fake, :middle...ddle=>:fake, :right=>:fake}, :bottom=>{:left=>:fake, :middle=>:fake, :right=>:fake}
+			true
+		else
 		false
+		end
 	end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,21 @@
 require_relative '../lib/board'
 
 describe 'A Tic Tac Toe board' do
+	
+	let(:full_board) {
+	    Board.new({
+	      top: {left: :fake, middle: :fake, right: :fake},
+	      middle: {left: :fake, middle: :fake, right: :fake},
+	      bottom: {left: :fake, middle: :fake, right: :fake}
+	    })
+	  }
 
-  it 'needs covered with tests' do
-    fail 'See spec/board_spec.rb'
+  it 'responds to locations' do
+    expect(full_board).to respond_to(:locations) 
+  end
+
+  it "has a string representation that is the board token representations" do
+    expect(full_board.to_s).to eq("fake|fake|fake\n-----\nfake|fake|fake\n-----\nfake|fake|fake\n")
   end
 
 end
